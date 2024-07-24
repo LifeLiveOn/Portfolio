997789aaa619ae9833b7f6409e7e30e36e78b26b
lifeliveon <tnkchaseme@gmail.com>
2024-07-24 12:28:07 -0500

    add mobile friendly nav bar

--- project/scripts.js
+++ project/scripts.js
@@ -10,3 +10,36 @@ function loadHTML(file, elementId) {
 // Load header and footer
 loadHTML('header.html', 'header');
 loadHTML('footer.html', 'footer');
+
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+      const mobileMenu = document.getElementById('mobile-menu');
+      const navLinks = document.querySelector('.nav__links');
+  
+      function checkScreenWidth() {
+          if (window.innerWidth <= 500) {
+              navLinks.style.display = 'none';
+              mobileMenu.style.display = 'block';
+          } else {
+              navLinks.style.display = 'flex';
+              mobileMenu.style.display = 'none';
+          }
+      }
+  
+      // Initial check on page load
+      checkScreenWidth();
+  
+      // Check on window resize
+      window.addEventListener('resize', checkScreenWidth);
+      window.addEventListener('load', checkScreenWidth);
+  });
+  
+

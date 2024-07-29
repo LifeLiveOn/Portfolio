4a3eab46ee0dcb9bc63b3affe8c0851f639766d5
Thanh <tnkchaseme@gmail.com>
2024-07-28 22:01:36 -0500

    update html css

--- project/scripts.js
+++ project/scripts.js
@@ -1,45 +1,38 @@
 function loadHTML(file, elementId) {
     fetch(file)
-        .then(response => response.text())
-        .then(data => {
+        .then((response) => response.text())
+        .then((data) => {
             document.getElementById(elementId).innerHTML = data;
         })
-        .catch(error => console.error('Error loading HTML:', error));
+        .catch((error) => console.error("Error loading HTML:", error));
 }
 
 // Load header and footer
-loadHTML('header.html', 'header');
-loadHTML('footer.html', 'footer');
-
+loadHTML("header.html", "header");
+loadHTML("footer.html", "footer");
 
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
-  }
-  
-  function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-  }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-      const mobileMenu = document.getElementById('mobile-menu');
-      const navLinks = document.querySelector('.nav__links');
-  
-      function checkScreenWidth() {
-          if (window.innerWidth <= 500) {
-              navLinks.style.display = 'none';
-              mobileMenu.style.display = 'block';
-          } else {
-              navLinks.style.display = 'flex';
-              mobileMenu.style.display = 'none';
-          }
-      }
-  
-      // Initial check on page load
-      checkScreenWidth();
-  
-      // Check on window resize
-      window.addEventListener('resize', checkScreenWidth);
-      window.addEventListener('load', checkScreenWidth);
-  });
-  
+}
 
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const mobileMenu = document.getElementById("mobile-menu");
+    const navLinks = document.querySelector(".nav__links");
+
+    function checkScreenWidth() {
+        if (window.innerWidth <= 500) {
+            navLinks.style.display = "none";
+            mobileMenu.style.display = "block";
+        } else {
+            navLinks.style.display = "flex";
+            mobileMenu.style.display = "none";
+        }
+    }
+
+    // Initial check on page load
+    checkScreenWidth();
+});
